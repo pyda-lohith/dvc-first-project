af2072c7a4d7d722d4de4cc08b8f21ef871a7a55
pyda-lohith <lohi.pyda@gmail.com>
2021-05-30 09:34:43 +0530

    apiresponse done

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import os
 import numpy as np
 import joblib
+import yaml
 
 params_path= "params.yaml"
 webapp_root = "webapp"
@@ -11,12 +12,42 @@ template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config= yaml.safe_load(yaml_file)
+    return config
+
+def predict(data):
+    config= read_params(params_path)
+    model_dir_path= config["webapp_model_dir"]
+    model= joblib.load(model_dir_path)
+    prediction= model.predict(data)
+    print(prediction[0])
+    return prediction[0]
+
+def api_response(request):
+    try:
+        print("reachedddddddddddddddddddddddddddddddddddddd api response")
+        data= np.array([list(request.json.values())])
+        response= predict(data)
+        response={"response": response}
+        return response
+    except Exception as e:
+        print(e)
+        error= {"error":"Something went wrong"}
+        return error    
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-
     if request.method == "POST":
-        pass
+        try:
+            if request.json:
+                response = api_response(request)
+                return jsonify(response)
+        except Exception as e:
+            print(e)
+            error= {"error":"Something went wrong"}
+            return render_template("404.html", error= error)
     else:
         return render_template("index.html")
 
